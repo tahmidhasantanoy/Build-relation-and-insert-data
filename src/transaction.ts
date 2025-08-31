4ff27135cc2c62711191b796861147ffda69af90
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2025-08-31 19:49:27 +0600

    Feat: Interactive transaction performed

--- src/transaction.ts
+++ src/transaction.ts
@@ -5,32 +5,70 @@ const prisma = new PrismaClient();
 console.log("Transaction Operation");
 
 const transactionOperation = async () => {
-  try {
-    const create = prisma.user.create({
+  // Batch transaction
+  //   try {
+  //     // query-1
+  //     const create = prisma.user.create({
+  //       data: {
+  //         username: "Tahmid Hasan",
+  //         email: "tahmid.hasan@example.com",
+  //         age: 30,
+  //       },
+  //     });
+
+  //     // query-2
+  //     const update = prisma.user.update({
+  //       where: {
+  //         id: 3,
+  //       },
+  //       data: {
+  //         age: 31,
+  //       },
+  //     });
+
+  //     // Batch transaction
+  //     const [createUser, updateUser] = await prisma.$transaction([
+  //       create,
+  //       update,
+  //     ]);
+  //     console.log(createUser, updateUser);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+
+  // interactive transaction
+  /* $ means api */
+  const interactiveTransaction = await prisma.$transaction(async (tx) => {
+    // Query-1
+    const createUser = await tx.user.create({
       data: {
-        username: "Tahmid Hasan",
-        email: "tahmid.hasan@example.com",
-        age: 30,
+        username: "Tahmid Hasan tanoy",
+        email: "tahmid.hasan.tanoy@example.com",
+        age: 24,
+        role: "admin",
       },
     });
 
-    const update = prisma.user.update({
+    // Query-2
+    const updateUser = await tx.user.update({
       where: {
         id: 3,
       },
       data: {
-        age: 31,
+        age: 25,
       },
     });
 
-    const [createUser, updateUser] = await prisma.$transaction([
-      create,
-      update,
-    ]);
-    console.log(createUser, updateUser);
-  } catch (err) {
-    console.log(err);
-  }
+    // Query-3
+    const deleteUser = await tx.user.delete({
+      where: {
+        id: 4,
+      },
+    });
+    return updateUser;
+  });
+
+  console.log(interactiveTransaction);
 };
 
 transactionOperation();
